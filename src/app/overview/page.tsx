70298563c9cf3799121fb91c4b2d842e8828a05b
Anesu Kabuku <125449219+kabukuanesu@users.noreply.github.com>
2024-07-18 21:33:02 +0200

    Overview done

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -9,50 +9,94 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  BellIcon,
+  XMarkIcon,
+  CalendarDaysIcon,
+  HandRaisedIcon,
+} from "@heroicons/react/24/outline";
+
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+  ChatBubbleLeftRightIcon,
+  ServerStackIcon,
+} from "@heroicons/react/20/solid";
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
+  name: "Anesu Kabuku",
+  email: "kabukuanesu@gmail.com",
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+  { name: "Home", href: "/home-page", current: false },
+  { name: "Overview", href: "/overview", current: true },
+  { name: "Upload", href: "/gemini-vision", current: false },
+  { name: "Diagnosis", href: "/gemini-chat", current: false },
+  { name: "Chatbot", href: "/gemini-chatting", current: false },
+  { name: "SkinCare", href: "/library", current: false },
+  { name: "History", href: "/history", current: false },
+  { name: "Notifications", href: "/reminder", current: false },
+  { name: "Community", href: "/community", current: false },
+  { name: "Help", href: "/support", current: false },
 ];
 const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Profile", href: "/profile" },
+  { name: "Settings", href: "/settings" },
+  { name: "Sign out", href: "/login" },
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+const features = [
+  {
+    name: "Upload An Image.",
+    description:
+      "An interface where you can upload pictures of skin lesions for analysis. Includes guidelines on taking and uploading clear images.",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "Symptoms Checker.",
+    description:
+      "An interface where you can input symptoms or describe skin issues, and the app provides possible conditions based on the input.",
+    icon: LockClosedIcon,
+  },
+  {
+    name: "Chatbot.",
+    description:
+      "Access to the chatbot for asking questions about skin cancer, other skin diseases, treatment options, and preventive measures.",
+    icon: ServerIcon,
+  },
+  {
+    name: "Skin Health Library.",
+    description:
+      "A resource section with articles, videos, and infographics about skin health, various skin conditions, treatment options, and preventive care.",
+    icon: ServerStackIcon,
+  },
+  {
+    name: "Community Forum.",
+    description:
+      "A space where you can join discussions, share your experiences, ask questions, and support each other regarding skin health issues.",
+    icon: ChatBubbleLeftRightIcon,
+  },
+];
+
 export default function Overview() {
   return (
     <div>
-      {/*
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
-*/}
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
+      <div className="min-h-full bg-white">
+        <Disclosure as="nav" className="bg-indigo-600">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
                   <img
-                    alt="Your Company"
+                    alt="Skin Cancer Detector"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     className="h-8 w-8"
                   />
@@ -198,16 +242,171 @@ export default function Overview() {
           </DisclosurePanel>
         </Disclosure>
 
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
-          </div>
-        </header>
         <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 bg-white">
+            <div className="overflow-hidden bg-white py-24 sm:py-32">
+              <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                  <div className="lg:pr-8 lg:pt-4">
+                    <div className="lg:max-w-lg">
+                      <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                        Overview
+                      </h2>
+                      <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        Application Features
+                      </p>
+                      <p className="mt-6 text-lg leading-8 text-gray-600">
+                        A description of the features provided.
+                      </p>
+                      <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                        {features.map((feature) => (
+                          <div key={feature.name} className="relative pl-9">
+                            <dt className="inline font-semibold text-gray-900">
+                              <feature.icon
+                                aria-hidden="true"
+                                className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                              />
+                              {feature.name}
+                            </dt>{" "}
+                            <dd className="inline">{feature.description}</dd>
+                          </div>
+                        ))}
+                      </dl>
+                    </div>
+                  </div>
+                  {/*<img
+                    alt="Product screenshot"
+                    src="/public/dashboard.png"
+                    width={2432}
+                    height={1442}
+                    className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                        />*/}
+
+                  <div className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0">
+                    <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+                      | How to use these features |
+                    </h2>
+                    <br />
+                    <p className="mt-4 text-black">
+                      <b>| TO DO IMAGE DIAGNOSIS |</b>
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Press the <b>Upload Image </b> tab |
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Drag or select the image to analyse |
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Write on the text input to get accurate results |
+                    </p>
+                    <p className="mt-4 text-black">| Press Enter |</p>
+                    <br />
+                    <p className="mt-4 text-black">
+                      <b>| TO USE SYMPTOMS CHECKER |</b>
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Press the <b>Symptoms Checker</b> tab |
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Write all the symptoms on the text area |
+                    </p>
+                    <p className="mt-4 text-black">| Press Enter |</p>
+                    <br />
+                    <p className="mt-4 text-black">
+                      <b>| TO ACCESS SKIN HEALTH LIBRARY |</b>
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Press the <b>Skin Health Library</b> tab |
+                    </p>
+                    <p className="mt-4 text-black">
+                      | Select the resource of your choice |
+                    </p>
+                    <p className="mt-4 text-black">| Press Enter |</p>
+                    <br />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                <div className="max-w-xl lg:max-w-lg">
+                  <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    Subscribe to our newsletter.
+                  </h2>
+                  <p className="mt-4 text-lg leading-8 text-gray-300">
+                    Subscribe to our daily newsletter to receive the latest news
+                    and updates on skincare.
+                  </p>
+                  <div className="mt-6 flex max-w-md gap-x-4">
+                    <label htmlFor="email-address" className="sr-only">
+                      Email address
+                    </label>
+                    <input
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      required
+                      placeholder="Enter your email"
+                      autoComplete="email"
+                      className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                    <button
+                      type="submit"
+                      className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                      Subscribe
+                    </button>
+                  </div>
+                </div>
+                <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                  <div className="flex flex-col items-start">
+                    <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                      <CalendarDaysIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-white"
+                      />
+                    </div>
+                    <dt className="mt-4 font-semibold text-white">
+                      Weekly articles
+                    </dt>
+                    <dd className="mt-2 leading-7 text-gray-400">
+                      Stay informed with our weekly updates, which include
+                      information on upcoming events and newly released
+                      articles.
+                    </dd>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                      <HandRaisedIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-white"
+                      />
+                    </div>
+                    <dt className="mt-4 font-semibold text-white">No spam</dt>
+                    <dd className="mt-2 leading-7 text-gray-400">
+                      We do not engage in spamming, and please be aware that by
+                      subscribing, you are agreeing to the terms and conditions
+                      of this platform.
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+            <div
+              aria-hidden="true"
+              className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6"
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+              />
+            </div>
           </div>
         </main>
       </div>
