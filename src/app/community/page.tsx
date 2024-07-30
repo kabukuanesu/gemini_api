f64c781df9d6708421bda3d359f8beda06a98147
Anesu Kabuku <125449219+kabukuanesu@users.noreply.github.com>
2024-07-30 14:01:45 +0200

    community page done

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -12,47 +12,145 @@ import {
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
+  name: "Anesu Kabuku",
+  email: "kabukuanesu@gmail.com",
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+  { name: "Home", href: "/home-page", current: false },
+  { name: "Overview", href: "/overview", current: false },
+  { name: "Upload", href: "/gemini-vision", current: false },
+  { name: "Diagnosis", href: "/gemini-chat", current: false },
+  { name: "Chatbot", href: "/gemini-chatter", current: false },
+  { name: "SkinCare", href: "/library", current: false },
+  { name: "History", href: "/history", current: false },
+  { name: "Notifications", href: "/reminder", current: false },
+  { name: "Community", href: "/community", current: true },
+  { name: "Help", href: "/support", current: false },
 ];
 const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Your Profile", href: "/profile" },
+  { name: "Settings", href: "/settings" },
+  { name: "Sign out", href: "/login" },
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+const people = [
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  // More people...
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "Boost your conversion rate",
+    href: "/library",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Medicine", href: "/library" },
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "/library",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  // More posts...
+  {
+    id: 2,
+    title: "Discover how to care for your skin daily.",
+    href: "/library",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Health", href: "/library" },
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "/library",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 3,
+    title: "Let's delve into a detailed discussion about skin cancer.",
+    href: "/library",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Health", href: "/library" },
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "/library",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+];
+
+const stats = [
+  { id: 1, name: "Active users every 24 hours", value: "44 million" },
+  { id: 2, name: "Patients under diagnosis", value: "19 million" },
+  { id: 3, name: "New users annually", value: "46,000" },
+];
+
 export default function Community() {
   return (
     <div>
-      {/*
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
-*/}
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-indigo-600">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
                   <img
-                    alt="Your Company"
+                    alt="Skin Cancer Detector"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     className="h-8 w-8"
                   />
@@ -198,16 +296,135 @@ export default function Community() {
           </DisclosurePanel>
         </Disclosure>
 
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
-          </div>
-        </header>
-        <main>
+        <main className="bg-white">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             {/* Your content */}
+            <div>
+              <div className="bg-white py-24 sm:py-32">
+                <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+                  <div className="max-w-2xl">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                      Meet our community supervisors.
+                    </h2>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                      If you have any questions or require support of any kind,
+                      please feel free to contact any of our supervisors through
+                      this platform.
+                    </p>
+                  </div>
+                  <ul
+                    role="list"
+                    className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+                  >
+                    {people.map((person) => (
+                      <li key={person.name}>
+                        <div className="flex items-center gap-x-6">
+                          <img
+                            alt=""
+                            src={person.imageUrl}
+                            className="h-16 w-16 rounded-full"
+                          />
+                          <div>
+                            <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                              {person.name}
+                            </h3>
+                            <p className="text-sm font-semibold leading-6 text-indigo-600">
+                              {person.role}
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="bg-white py-24 sm:py-4">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                  <div className="mx-auto max-w-2xl lg:mx-0">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                      Our Community Forums
+                    </h2>
+                    <p className="mt-2 text-lg leading-8 text-gray-600">
+                      Select any community forum that interests you and feel
+                      free to participate.
+                    </p>
+                  </div>
+                  <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    {posts.map((post) => (
+                      <article
+                        key={post.id}
+                        className="flex max-w-xl flex-col items-start justify-between"
+                      >
+                        <div className="flex items-center gap-x-4 text-xs">
+                          <time
+                            dateTime={post.datetime}
+                            className="text-gray-500"
+                          >
+                            {post.date}
+                          </time>
+                          <a
+                            href={post.category.href}
+                            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                          >
+                            {post.category.title}
+                          </a>
+                        </div>
+                        <div className="group relative">
+                          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                            <a href={post.href}>
+                              <span className="absolute inset-0" />
+                              {post.title}
+                            </a>
+                          </h3>
+                          <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                            {post.description}
+                          </p>
+                        </div>
+                        <div className="relative mt-8 flex items-center gap-x-4">
+                          <img
+                            alt=""
+                            src={post.author.imageUrl}
+                            className="h-10 w-10 rounded-full bg-gray-50"
+                          />
+                          <div className="text-sm leading-6">
+                            <p className="font-semibold text-gray-900">
+                              <a href={post.author.href}>
+                                <span className="absolute inset-0" />
+                                {post.author.name}
+                              </a>
+                            </p>
+                            <p className="text-gray-600">{post.author.role}</p>
+                          </div>
+                        </div>
+                      </article>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                  <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+                    {stats.map((stat) => (
+                      <div
+                        key={stat.id}
+                        className="mx-auto flex max-w-xs flex-col gap-y-4"
+                      >
+                        <dt className="text-base leading-7 text-gray-600">
+                          {stat.name}
+                        </dt>
+                        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                          {stat.value}
+                        </dd>
+                      </div>
+                    ))}
+                  </dl>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
