490ec044dfa947f4e94f92c626fe684434ee4979
Anesu Kabuku <125449219+kabukuanesu@users.noreply.github.com>
2024-07-30 13:21:43 +0200

    profile page done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,47 +12,57 @@ import {
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
+  name: "Anesu Kabuku",
+  email: "kabukuanesu@gmail.com",
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+  { name: "Home", href: "/home-page", current: false },
+  { name: "Overview", href: "/overview", current: false },
+  { name: "Upload", href: "/gemini-vision", current: false },
+  { name: "Diagnosis", href: "/gemini-chat", current: false },
+  { name: "Chatbot", href: "/gemini-chatter", current: false },
+  { name: "SkinCare", href: "/library", current: false },
+  { name: "History", href: "/history", current: false },
+  { name: "Notifications", href: "/reminder", current: false },
+  { name: "Community", href: "/community", current: false },
+  { name: "Help", href: "/support", current: false },
 ];
 const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Your Profile", href: "/profile" },
+  { name: "Settings", href: "/settings" },
+  { name: "Sign out", href: "/login" },
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+const links = [
+  { name: "My Dashboard", href: "/home-page" },
+  { name: "Overview", href: "/overview" },
+  { name: "My Library", href: "/library" },
+  { name: "My History", href: "/history" },
+];
+const stats = [
+  { name: "Notifications", value: "9+" },
+  { name: "Unread Emails", value: "6" },
+  { name: "Updates", value: "8" },
+  { name: "Community Forum", value: "9+" },
+];
+
 export default function Profile() {
   return (
     <div>
-      {/*
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
-*/}
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-indigo-600">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
                   <img
-                    alt="Your Company"
+                    alt="Skin Cancer Detector"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     className="h-8 w-8"
                   />
@@ -198,18 +208,118 @@ export default function Profile() {
           </DisclosurePanel>
         </Disclosure>
 
-        <header className="bg-white shadow">
+        <main className="bg-white">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+            <div>
+              <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+                <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+                <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+                <div className="mx-auto max-w-2xl lg:max-w-4xl">
+                  <h1>My Profile</h1>
+                  <figure className="mt-10">
+                    <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+                      <p>
+                        “It is often said that there is no cure for cancer;
+                        however, the key to surviving cancer lies in early
+                        detection. This platform offers tools to assist in the
+                        early diagnosis of skin cancer, thereby improving
+                        survival rates.”
+                      </p>
+                    </blockquote>
+                    <figcaption className="mt-10">
+                      <img
+                        alt=""
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        className="mx-auto h-10 w-10 rounded-full"
+                      />
+                      <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                        <div className="font-semibold text-gray-900">
+                          Judith Black
+                        </div>
+                        <svg
+                          width={3}
+                          height={3}
+                          viewBox="0 0 2 2"
+                          aria-hidden="true"
+                          className="fill-gray-900"
+                        >
+                          <circle r={1} cx={1} cy={1} />
+                        </svg>
+                        <div className="text-gray-600">CEO of Workcation</div>
+                      </div>
+                    </figcaption>
+                  </figure>
+                </div>
+              </section>
+            </div>
           </div>
         </main>
+        <div>
+          <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+              className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+            />
+            <div
+              aria-hidden="true"
+              className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+              />
+            </div>
+            <div
+              aria-hidden="true"
+              className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+              />
+            </div>
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl lg:mx-0">
+                <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                  Work with us
+                </h2>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  Our goal is to assist you by offering resources that promote
+                  healthy, cancer-free skin. Stay informed to receive the latest
+                  updates on skincare resources.
+                </p>
+              </div>
+              <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+                  {links.map((link) => (
+                    <a key={link.name} href={link.href}>
+                      {link.name} <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  ))}
+                </div>
+                <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+                  {stats.map((stat) => (
+                    <div key={stat.name} className="flex flex-col-reverse">
+                      <dt className="text-base leading-7 text-gray-300">
+                        {stat.name}
+                      </dt>
+                      <dd className="text-2xl font-bold leading-9 tracking-tight text-white">
+                        {stat.value}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
